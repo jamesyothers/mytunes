ccde387f5ab433f833d2fdc822df0ac0587e00d1
Hack Reactor Students <james.yothers@gmail.com>
2014-06-18 22:00:34 -0700

    On the way to greatness (take 3)

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,31 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.render();
+
+    // event listeners for our queue
+    this.model.on('add', function() {
+      this.render();
+    });
+
+    this.model.on('remove', function() {
+      this.render();
+    });
+
   },
 
-  render: function() {
-    return this.$el;
+  render: function(){
+    // same as library.view
+    this.$el.children().detach();
+
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
